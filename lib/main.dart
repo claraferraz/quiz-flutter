c4c20128cad8d26b3e2846d1b23381bc0c7d2ef7
Clara Ferraz <clara.ferraz@compasso.com.br>
2025-05-14 13:39:04 -0300

    aula column

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(PerguntasApp());
+  runApp(PerguntaApp());
 }
 
-class PerguntasApp extends StatelessWidget {
+class PerguntaApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
     //para novas instâncias eu não preciso colocar o nome 'new'
-    return MaterialApp(home: Text('Olá Flutter!'));
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Perguntas')),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[0]),
+            ElevatedButton(onPressed: null, child: Text('resposta 1')),
+            ElevatedButton(onPressed: null, child: Text('resposta 2')),
+            ElevatedButton(onPressed: null, child: Text('resposta 3')),
+            //o professor usou RaisedButton, mas está depreciado e foi substituído pelo ElevatedButton
+          ],
+        ),
+      ),
+    );
   }
 }
