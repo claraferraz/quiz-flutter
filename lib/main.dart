a10c4634704f0feda1eca0b2113f121e6eeee95c
Clara Ferraz <clara.ferraz@compasso.com.br>
2025-05-14 13:57:59 -0300

    funcoes dos buttons

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,10 @@ main() {
 }
 
 class PerguntaApp extends StatelessWidget {
+  void responder() {
+    print('pergunta respondida!');
+  }
+
   @override
   Widget build(BuildContext context) {
     final List<String> perguntas = [
@@ -20,9 +24,16 @@ class PerguntaApp extends StatelessWidget {
           children: <Widget>[
             Text(perguntas[0]),
             ElevatedButton(onPressed: null, child: Text('resposta 1')),
-            ElevatedButton(onPressed: null, child: Text('resposta 2')),
-            ElevatedButton(onPressed: null, child: Text('resposta 3')),
             //o professor usou RaisedButton, mas está depreciado e foi substituído pelo ElevatedButton
+            //button é disabled by default, ele ativa quando setamos um onPressed != null
+            ElevatedButton(
+              onPressed: () => responder(), //pode ser assim
+              child: Text('resposta 2'),
+            ),
+            ElevatedButton(
+              onPressed: responder, //ou pode ser assim
+              child: Text('resposta 3'),
+            ),
           ],
         ),
       ),
